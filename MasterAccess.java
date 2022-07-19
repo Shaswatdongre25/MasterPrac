0a114f245cb809490868fd698ad69757e1c3458a
Shaswatdongre25 <44455478+Shaswatdongre25@users.noreply.github.com>
2022-07-19 20:41:44 +0530

    2:

--- MasterAccess.java
+++ MasterAccess.java
@@ -1,4 +1,4 @@
-
+/* 
 //1: WAP to Access through instantiation
 class A{
     
@@ -21,4 +21,31 @@ class B{
     void pro(){
         System.out.println("hello");
     }
+} */
+
+//================================================================
+
+
+
+//2:WAP to access through inheritance
+
+/* class A extends B{
+    public static void main(String[] args){
+
+        A x = new A();
+        x.pro();
+        System.out.println(x.y);
+    }
 }
+
+class B{
+    int y = 20;
+
+    void pro(){
+        System.out.println("This is inside B")
+    }
+} */
+
+
+//=============================================================================
+
