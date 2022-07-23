e61966ea4e7db5ebdd612c7afe08f4d9cf3e9d75
Shaswatdongre25 <44455478+Shaswatdongre25@users.noreply.github.com>
2022-07-23 15:46:40 +0530

    MasterResolve

---
+++ MasterResolve.java
@@ -0,0 +1,43 @@
+/* class A
+{
+    public static void main(String[] args)
+    {
+        B x = new B();
+        x.pro();
+        System.out.println(x.y);
+    }
+}
+
+class B
+{
+    int y = 90;
+    
+    void pro()
+    {
+        System.out.println("Hello this is inside class B");
+
+    }
+} */
+
+
+
+
+/* class A extends B
+{
+    public static void main(String[]args)
+    {
+        A x = new A();
+        x.pro();
+        System.out.println(x.y);
+    }
+}
+
+class B
+{
+    int y = 90;
+
+    void pro()
+    {
+        System.out.println("Hello this is inside B void method");
+    }
+} */
