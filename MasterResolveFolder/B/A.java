7e6e32614eeafd7b1b5adbec90e3c150e7dee459
Shaswatdongre25 <44455478+Shaswatdongre25@users.noreply.github.com>
2022-07-27 09:44:48 +0530

    MasterResolveFolderB

---
+++ MasterResolveFolder/B/A.java
@@ -0,0 +1,8 @@
+class A{
+    public static void main(String[] args){
+        B x = new B();
+
+        x.pro();
+    }
+
+}
---
+++ MasterResolveFolder/B/B.java
@@ -0,0 +1,9 @@
+class B{
+    void pro(){
+      C y = new C();
+      System.out.println("This is inside B class");
+      
+      y.info();
+    }
+
+}
