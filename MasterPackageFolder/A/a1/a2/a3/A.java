252f0464f126dc17655f66607eaa1d97a44be54d
Shaswatdongre25 <44455478+Shaswatdongre25@users.noreply.github.com>
2022-07-28 15:42:51 +0530

    MasterPackageFolder/A

---
+++ MasterPackageFolder/A/a1/a2/a3/A.java
@@ -0,0 +1,15 @@
+package a1.a2.a3;
+
+import b1.b2.B;
+
+class A {
+
+    public static void main(String[] args){
+        B x = new B();
+
+       // x.pro();
+        System.out.println(x.t);
+
+
+    }
+}
